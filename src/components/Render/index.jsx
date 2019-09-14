422aba731757613bc761e669c3001388abc13ecb
jamiebeverley <jamie_beverley@hotmail.com>
2019-09-14 11:51:01 -0400

    popout button icon

--- src/components/Render/index.jsx
+++ src/components/Render/index.jsx
@@ -60,7 +60,9 @@ export default class Render extends PureComponent {
     render() {
         return (
             <div className="Render">
-                <button onClick={this.popoutRender.bind(this)}>popout</button>
+                <svg onClick={this.popoutRender.bind(this)} xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M19 19H5V5h7V3H5c-1.11 0-2 .9-2 2v14c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2v-7h-2v7zM14 3v2h3.59l-9.83 9.83 1.41 1.41L19 6.41V10h2V3h-7z"/>
+                </svg>
                 <iframe ref={this.iframeRef} src={"/render"}/>
             </div>
         )
