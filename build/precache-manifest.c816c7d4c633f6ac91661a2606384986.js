46c69d72c350b3b91293489817ea8331398ed0b8
jamiebeverley <jamie_beverley@hotmail.com>
2019-11-13 17:43:22 -0500

    build and homepage

---
+++ build/precache-manifest.c816c7d4c633f6ac91661a2606384986.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cfd96253181f6a60d72373d6e8c1de73",
+    "url": "/index.html"
+  },
+  {
+    "revision": "096b56073dc9218c4b89",
+    "url": "/static/css/main.6db8da22.chunk.css"
+  },
+  {
+    "revision": "d8b514c5373dc65114be",
+    "url": "/static/js/2.18c5ad9c.chunk.js"
+  },
+  {
+    "revision": "096b56073dc9218c4b89",
+    "url": "/static/js/main.0ac3151d.chunk.js"
+  },
+  {
+    "revision": "dbe422e9d57b35d41131",
+    "url": "/static/js/runtime~main.d65717a8.js"
+  },
+  {
+    "revision": "e1075641b0787e3c1067b6be7f832285",
+    "url": "/static/media/3.e1075641.PNG"
+  }
+]);
