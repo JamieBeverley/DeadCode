ecbe2b4b7f6e62bb6e7b5bf2c73abdd126870a96
JamieBeverley <jamie_beverley@hotmail.com>
2020-04-04 20:01:33 -0400

    title

--- src/components/App/index.jsx
+++ src/components/App/index.jsx
@@ -5,6 +5,7 @@ import SplitterLayout from 'react-splitter-layout';
 import './index.css';
 import Header from '../../containers/Header'
 import 'react-splitter-layout/lib/index.css'
+import Helmet from 'react-helmet'
 
 export default class App extends Component {
 
@@ -21,41 +22,43 @@ export default class App extends Component {
         return (
             <div
                 ref={this.appRef} className='App' tabIndex="0" onKeyDown={this.macros.bind(this)}>
-                <Header/>
-                <div id={'contentContainer'}>
-                    <SplitterLayout>
-                        <LaunchSpace/>
-                        <div style={{height: '100%'}}>
-                            <Flyout/>
-                            {/*<div className={'renderView'}>*/}
-                            {/*    <div></div>*/}
-                            {/*    <iframe style={{*/}
-                            {/*        height: "100%",*/}
-                            {/*        width: "100%",*/}
-                            {/*        border: 'none'*/}
-                            {/*    }} src={"/render"}/>*/}
+                <Helmet title={"deadcode"}>
+                    <Header/>
+                    <div id={'contentContainer'}>
+                        <SplitterLayout>
+                            <LaunchSpace/>
+                            <div style={{height: '100%'}}>
+                                <Flyout/>
+                                {/*<div className={'renderView'}>*/}
+                                {/*    <div></div>*/}
+                                {/*    <iframe style={{*/}
+                                {/*        height: "100%",*/}
+                                {/*        width: "100%",*/}
+                                {/*        border: 'none'*/}
+                                {/*    }} src={"/render"}/>*/}
+                                {/*</div>*/}
+                            </div>
+                            {/*<SplitterLayout vertical={false}>*/}
+                            {/*    <Flyout/>*/}
+                            {/*    <div className={'renderView'}>*/}
+                            {/*        <div></div>*/}
+                            {/*<iframe style={{*/}
+                            {/*    height: "100%",*/}
+                            {/*    width:"100%",*/}
+                            {/*    border:'none'*/}
+                            {/*}} src={"/render"}/>*/}
                             {/*</div>*/}
-                        </div>
-                        {/*<SplitterLayout vertical={false}>*/}
-                        {/*    <Flyout/>*/}
-                        {/*    <div className={'renderView'}>*/}
-                        {/*        <div></div>*/}
-                        {/*<iframe style={{*/}
-                        {/*    height: "100%",*/}
-                        {/*    width:"100%",*/}
-                        {/*    border:'none'*/}
-                        {/*}} src={"/render"}/>*/}
-                        {/*</div>*/}
-                        {/*</SplitterLayout>*/}
-                    </SplitterLayout>
-                </div>
-                {/*<div style={{height:'100%'}}>*/}
-                {/*    <LaunchSpace style={{width: this.state.divider + "%"}}/>*/}
-                {/*    <div id={"rightPanel"} style={{width: 100 - this.state.divider + "%"}}>*/}
-                {/*        <Flyout style={{height: this.state.horizontalDivider + "%"}}/>*/}
-                {/*        <iframe style={{height:(100-this.state.horizontalDivider) + "%",border:'1pt solid var(--stem-on)',borderBottom:'none',borderRight:'none'}} src={"/render"}/>*/}
-                {/*    </div>*/}
-                {/*</div>*/}
+                            {/*</SplitterLayout>*/}
+                        </SplitterLayout>
+                    </div>
+                    {/*<div style={{height:'100%'}}>*/}
+                    {/*    <LaunchSpace style={{width: this.state.divider + "%"}}/>*/}
+                    {/*    <div id={"rightPanel"} style={{width: 100 - this.state.divider + "%"}}>*/}
+                    {/*        <Flyout style={{height: this.state.horizontalDivider + "%"}}/>*/}
+                    {/*        <iframe style={{height:(100-this.state.horizontalDivider) + "%",border:'1pt solid var(--stem-on)',borderBottom:'none',borderRight:'none'}} src={"/render"}/>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+                </Helmet>
             </div>
         )
     }
